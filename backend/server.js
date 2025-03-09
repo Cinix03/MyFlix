3d92f6b379ef4294f948108fc9925e415949b949
George <vasileirinel013@gmail.com>
2025-03-09 23:50:50 +0200

    signup validation

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js"
+import { ENV_VARS } from './config/envVars.js';
+import { connectDB } from './config/db.js';
+
+const app = express();
+const PORT = ENV_VARS.PORT
+
+dotenv.config();
+
+console.log("Mongo_URI: ", process.env.MONGO_URI)
+
+
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server started at  http://localhost:" + PORT);
+    connectDB();
+})
