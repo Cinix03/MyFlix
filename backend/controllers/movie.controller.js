b3568e91c76ff1ce8a785e0bbe8aff64250baf88
George <vasileirinel013@gmail.com>
2025-03-11 20:39:02 +0200

    rute pentru details, trailers, similar movies

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -15,7 +15,43 @@ export async function getMovieTrailers(req, res){
     const { id } = req.params;
     try{
         const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`);
-        res.json({success:true, trailers: data.results});
+        res.status(200).json({success:true, trailers: data.results});
+    }catch(error){
+        if(error.message.includes("404"))
+        {
+            return res.status(404).send(null);
+        }
+        console.log("Error in getMovieTrailers: ", error.message);
+        res.status(500).json({
+            success:false,
+            message: "Internal Server Error"
+        });
+    }
+}
+
+export async function getMovieDetails(req, res){
+    const { id } = req.params;
+    try{
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}?language=en-US`);
+        res.status(200).json({success:true, movie: data});
+    }catch(error){
+        if(error.message.includes("404"))
+        {
+            return res.status(404).send(null);
+        }
+        res.status(500).json({
+            success:false,
+            message: "Internal Server Error"
+        });
+    }
+}
+
+
+export async function getSimilarMovies(req, res){
+    const { id } = req.params;
+    try{
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`);
+        res.status(200).json({success:true, movies: data.results});
     }catch(error){
         if(error.message.includes("404"))
         {
