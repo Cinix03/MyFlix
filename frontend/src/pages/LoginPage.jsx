23cf1975431e61f69d6f4682ebb0afa69a84e29e
George <vasileirinel013@gmail.com>
2025-03-20 11:01:29 +0200

    nu merge

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 
 const LoginPage = () => {
@@ -12,11 +13,11 @@ const LoginPage = () => {
 
       <div className="flex items-center justify-center mt-20 mx-3">
         <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
-          <h1 className="tex-center text-white text-2xl font-bold mb-4">Log in</h1>
+          <h1 className="text-center text-white text-2xl font-bold mb-4">Log in</h1>
           <form className="space-y-4">
             <div>
               <label htmlFor="email" className="text-sm font-medium text-gray-300 block">Email</label>
-              <input type="email" className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focurs:outline-none:ring" placeholder="email" id="email" />
+              <input type="email" className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none:ring" placeholder="email" id="email" />
             </div>
             <div>
               <label htmlFor="password" className="text-sm font-medium text-gray-300 block">Password</label>
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -8,6 +8,7 @@ const SignUpPage = () => {
 
   const handleSignUp = (e) => {
     e.preventDefault();
+    console.log("Sign Up");
     console.log(email, username, password);
   }
 
@@ -22,11 +23,11 @@ const SignUpPage = () => {
 
       <div className="flex items-center justify-center mt-20 mx-3">
         <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
-          <h1 className="tex-center text-white text-2xl font-bold mb-4">Sign Up</h1>
+          <h1 className="text-center text-white text-2xl font-bold mb-4">Sign Up</h1>
           <form className="space-y-4" onSubmit={handleSignUp}>
             <div>
               <label htmlFor="email" className="text-sm font-medium text-gray-300 block">Email</label>
-              <input type="email" className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focurs:outline-none:ring" placeholder="email" id="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+              <input type="email" className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none:ring" placeholder="email" id="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
             </div>
             <div>
               <label htmlFor="username" className="text-sm font-medium text-gray-300 block">Username</label>
@@ -36,7 +37,7 @@ const SignUpPage = () => {
               <label htmlFor="password" className="text-sm font-medium text-gray-300 block">Password</label>
               <input type="password" className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focurs:outline-none:ring" placeholder="password" id="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
             </div>
-            <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md transition duration-300 hover:bg-green-900 hover:shadow-lg hover:scale-105">
+            <button type="submit" className="w-full py-2 bg-red-600 text-white font-semibold rounded-md transition duration-300 hover:bg-green-900 hover:shadow-lg hover:scale-105">
               Sign Up
             </button>
             <div className='text-center text-gray-300'>
