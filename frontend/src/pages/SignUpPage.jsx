dca673351d134f10716319c068a8ba4269dd30b2
George <vasileirinel013@gmail.com>
2025-03-20 09:34:29 +0200

    input controlat

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,16 @@
 import { Link } from "react-router-dom";
 
 const SignUpPage = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    console.log(email, username, password);
+  }
+
+
   return (
     <div className="h-screen w-full home-bg">
       <header className="max-w-6xl mx-auto flex items-center justify-start p-4 bg-transparent">
@@ -12,10 +22,10 @@ const SignUpPage = () => {
       <div className="flex items-center justify-center mt-20 mx-3">
         <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
           <h1 className="tex-center text-white text-2xl font-bold mb-4">Sign Up</h1>
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSignUp}>
             <div>
               <label htmlFor="email" className="text-sm font-medium text-gray-300 block">Email</label>
-              <input type="email" className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focurs:outline-none:ring" placeholder="email" id="email" />
+              <input type="email" className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focurs:outline-none:ring" placeholder="email" id="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
             </div>
             <div>
               <label htmlFor="username" className="text-sm font-medium text-gray-300 block">Username</label>
